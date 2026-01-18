66a9a05ff67355c29fea07ef75bf86f5181843cf
grzegorz <grzegorz.slomka@studenci.collegiumwitelona.pl>
2026-01-18 21:15:23 +0100

    Improve game architecture

--- src/Game/Game.php
+++ src/Game/Game.php
@@ -2,26 +2,33 @@
 
 namespace Game;
 
-use Player\PlayerFactory;
 use Dungeon\Dungeon;
 use Combat\Battle;
 use Input\CliInputHandler;
+use Character\Player;
 
 final class Game
 {
     public function run(): void
     {
-        $player = PlayerFactory::create();
+        echo "=== PHP RPG ===\n";
+
+        $player = Player::createFromChoice();
         $dungeon = new Dungeon();
         $input = new CliInputHandler();
 
-        while ($room = $dungeon->nextRoom()) {
-            if ($room->hasEnemy()) {
-                $battle = new Battle($input);
-                $battle->fight($player, $room->getEnemy());
+        foreach ($dungeon->getRooms() as $room) {
+            echo "\n>>> Nowy pokój\n";
+
+            $battle = new Battle($input);
+            $battle->fight($player, $room->getEnemy());
+
+            if (!$player->isAlive()) {
+                echo "Przegrałeś grę.\n";
+                return;
             }
         }
 
-        echo "Koniec gry\n";
+        echo "\n Pokonałeś loch!\n";
     }
 }
