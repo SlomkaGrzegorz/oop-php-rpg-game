0995100b852dd796229ab885c7b7c8dade93d9f9
JakubSzpotowicz <jakub.szpotowicz@studenci.collegiumwitelona.pl>
2026-01-18 23:50:06 +0100

    Players

---
+++ Character/Player.php
@@ -0,0 +1,5 @@
+<?php
+class Player extends Character {
+    public function __construct(string $Name, int $HP, int $Damage, int $Armor) {
+    }
+}
---
+++ Character/players.php
@@ -0,0 +1,4 @@
+<?php
+
+$gracz1 = new Player("Gracz1", 150, 20, 20);
+$gracz2 = new Player("Gracz2", 300, 10, 15);
