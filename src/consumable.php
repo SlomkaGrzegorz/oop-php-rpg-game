6044158d9446474af4abc5ccc181345cad3a7848
MichalOlszowy <michal.olszowy@studenci.collegiumwitelona.pl>
2026-01-19 01:48:35 +0100

    testowe itemki obu podgrup(jeszcze armor bo mi sie zapomniało xd)

--- src/consumable.php
+++ src/consumable.php
@@ -1,1 +1,8 @@
 <?php
+class consumable extends items{
+    protected int $usageNumber;
+
+    public function Use($player): void{
+        $this->usageNumber--;
+    }
+}
---
+++ src/hp potion.php
@@ -0,0 +1,18 @@
+<?php
+class hpPotion extends consumable{
+    protected string $name = "Mikstura zdrowia";
+    protected string $description = "Mikstura odnawiająca od 2 do 5 zdrowia";
+    protected int $usageNumber = 1;
+    protected int $healAmount;
+
+    public function Use($player): void
+    {
+        parent::Use($player);
+
+        $this->healAmount = rand(2, 5);
+        $player->hp += $this->healAmount;
+
+        echo "Użyłeś {$this->name}. Odzyskałeś {$this->healAmount} HP.\n";
+    }
+
+}
